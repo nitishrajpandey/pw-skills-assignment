4dc189dba38b3d1c74509241def0e1ae0c3e269f
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-08-17 10:40:44 +0530

    question 10 complete

---
+++ JavaScript Fundamentals - 1 assigment/question-10/question-10.js
@@ -0,0 +1,7 @@
+const array1=[21,32,12,-12,43,-31,-11,-34,97,-17,-22,-634,121];
+for (let index = 0; index < array1.length; index++) {
+    if (array1[index]>0) {
+        console.log(array1[index]); 
+    }
+    
+}
