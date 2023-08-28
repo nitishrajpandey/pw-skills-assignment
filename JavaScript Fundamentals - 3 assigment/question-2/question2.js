cbb908bc1b5ff60e74612782a92f35da4ee50a1b
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-08-28 08:10:10 +0530

    question 2 complete

---
+++ JavaScript Fundamentals - 3 assigment/question-2/question2.js
@@ -0,0 +1,13 @@
+let students={
+    name:"nitish",
+    age:21,
+    grade:"A"
+}
+
+function chageValue(value) {
+   students.grade=value
+return students
+}
+let result=chageValue("A++");
+console.log(result);
+
