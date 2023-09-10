ce1a2c1c0553fb2123fc05f139a4103393f7ca2c
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-09-10 08:20:38 +0530

    question 4 complete

---
+++ javascript functional program/question-4/script.js
@@ -0,0 +1,15 @@
+if(true){  
+
+    console.log(var1);  
+    console.log(var2); 
+    console.log(var3); 
+
+    let var1 = "nitish";
+    var var2 = "raj"
+    const var3 = 123;
+
+    
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+}
